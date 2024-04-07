39b2966932e34e1ac3fba2846c7a9713a6fcc573
kimyougmin <dudals896@gmail.com>
2024-04-07 14:05:50 +0900

    coinTitle 조건부 랜더링 적용

--- expected_cryptocurrency/src/mainScreen/components/CoinTitle.tsx
+++ expected_cryptocurrency/src/mainScreen/components/CoinTitle.tsx
@@ -1,17 +1,55 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Coin from "../../typs/Coin";
-import '../style/CoinTitle.css'
+import '../style/CoinTitle.css';
+import arrowDown from "../img/free-icon-downward-arrow-2268476.png";
+import arrowUp from "../img/free-icon-upward-arrow-2268475.png";
 
 function CoinTitle({name, price, iconUrl, symbol, change}: Coin) {
-    const [color, setColor] = useState<string>('');
-    useEffect(() => {
-        if (parseFloat(change) > 0)
-        {
-            setColor('red')
+    const changeManager = () => {
+        if (parseFloat(change) > 0) {
+            return (
+                <div className='coinTitleBottom'>
+                    <div className='coinTitleBottom-top'>
+                        <h3 className='price' style={{color: 'red'}}>{price}</h3>
+                        <p style={{color: 'red'}}>dollar</p>
+                    </div>
+                    <div className='coinTitleBottom-bottom'>
+                        <p className='change' style={{color: 'red'}}>+ {change}%</p>
+                        <div style={{display: "flex"}}>
+                            <img className='arrowImg' src={arrowUp}/>
+                            <p style={{color: 'red'}}>{parseFloat(price) * parseFloat(change)}</p>
+                        </div>
+                    </div>
+                </div>
+                )
+        } else if (parseFloat(change) < 0) {
+            return (
+                <div className='coinTitleBottom'>
+                    <div>
+                        <h3 className='price' style={{color: 'blue'}}>{price}</h3>
+                        <p style={{color: 'blue'}}>dollar</p>
+                    </div>
+                    <div className='coinTitleBottom-bottom'>
+                        <p className='change' style={{color: 'blue'}}>- {change}%</p>
+                        <div style={{display: "flex"}}>
+                            <img className='arrowImg' src={arrowDown}/>
+                            <p style={{color: 'red'}}>{parseFloat(price) * parseFloat(change)}</p>
+                        </div>
+                    </div>
+                </div>
+            )
         } else {
-            setColor('blue')
+            return (
+                <div className='coinTitleBottom'>
+                    <div>
+                        <h3 className='price' style={{color: 'black'}}>{price}</h3>
+                        <p style={{color: 'black'}}>dollar</p>
+                    </div>
+                    <p className='change' style={{color: 'black'}}>{change}%</p>
+                </div>
+            )
         }
-    });
+    };
     return (
         <div>
             <div className='coinTitleHeader'>
@@ -19,10 +57,7 @@ function CoinTitle({name, price, iconUrl, symbol, change}: Coin) {
                 <h3 className='name'>{name}</h3>
                 <p className='symbol'>{symbol}</p>
             </div>
-            <div className='coinTitleBottom'>
-                <p className='price' style={{color: color}}>{price}</p>
-                <p className='change' style={{color: color}}>{change}</p>
-            </div>
+            {changeManager()}
         </div>
     );
 }
